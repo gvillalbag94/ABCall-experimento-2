0c4d4ace47feab07b2526beb554216f1d7f198cf
Santiago <s.patino@uniandes.edu.co>
2024-09-26 20:34:54 -0500

    Ajuste comentarios app

--- autorizador/__init__.py
+++ autorizador/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
 def create_app(config_name):
+    
     app = Flask(__name__)
+    
     # Configurar el SECRET_KEY para JWT
     app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+    
+    # Configurar el ALGORITHM para JWT
     app.config['ALGORITHM'] = 'HS256'
+    
     return app
--- autorizador/app.py
+++ autorizador/app.py
@@ -4,7 +4,6 @@ import requests
 import jwt
 import datetime
 
-
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
@@ -13,7 +12,7 @@ app_context.push()
 SECRET_KEY = app.config['JWT_SECRET_KEY']
 ALGORITHM = app.config['ALGORITHM']
 
-# Usuario simulado (en una aplicación real, validarías las credenciales contra una base de datos)
+# Usuario simulado
 users = {
     'user1': '1234'
 }
@@ -21,41 +20,42 @@ users = {
 
 @app.route('/validate', methods=['POST'])
 def validate_user():
-    """
-    Function para validar el usuario con sus respectivas caracteristicas
-    """
+    # Obtiene las credenciales a validar
     user_data = request.get_json()
     username = user_data.get('username')
     password = user_data.get('password')
 
-    # Validar las credenciales (simulación)
+    # Validar las credenciales
     if username in users and users[username] == password:
         # Generar JWT
+        # Almacena el nombre del usuario que lo genero y la fecha de expiracion (1 hora)
         token = jwt.encode({
             'username': username,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
         }, SECRET_KEY, algorithm=ALGORITHM)
+        
+        # Devuelve el token al API_Gateway
         return jsonify({'token': token}), 200
     else:
+        # Indica si las credenciales son invalidas
         return jsonify({'error': 'Invalid credentials'}), 401
     
-
+# Valida el estado del token
 @app.route('/validateToken', methods=['POST'])
 def validate_token():
-    """
-    Function para realizar validacion del token cifrandolo.
-    """
+    # Recibe el token
     token_recibido = request.get_json()
     token = None
     if token_recibido:
-        # El token suele venir en formato "Bearer <token>", extraemos el token
+        # Se extrae el token del formato "Bearer <token>"
         token = token_recibido.split(' ')[1]
     # Guardar el token en una variable
     token_guardado = token
-    # Puedes decodificar el token aquí si lo necesitas
-    # Por ejemplo:
+    # Se descifra el token a partir de la SECRET_KEY
     try:
+        # Si logra decifrarlo, el token es correcto
         payload = jwt.decode(token_guardado, SECRET_KEY, algorithms=[ALGORITHM])
         return jsonify({'mensaje': 'Token válido', 'payload': payload}), 200
     except jwt.InvalidTokenError:
+        # Si falla en decifrarlo, el token ha sido adulterado
         return jsonify({'mensaje': 'Token inválido o modificado'}), 401
