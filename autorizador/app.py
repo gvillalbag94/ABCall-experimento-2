231e8d52797ddc3ee5cd07d19a1f0b6ddf4552ef
nicbot23 <n.ibarra@uniandes.edu.co>
2024-09-26 19:53:35 -0500

    sea grega la funcion para validar el token y realizar el cifrado del mismo para verificar

--- autorizador/app.py
+++ autorizador/app.py
@@ -3,22 +3,29 @@ from autorizador import create_app
 import requests
 import jwt
 import datetime
+
+
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
-# Clave secreta para firmar el JWT
+
 SECRET_KEY = app.config['JWT_SECRET_KEY']
 ALGORITHM = app.config['ALGORITHM']
-# Usuario simulado (en una aplicación real, validarías las credenciales contra una base de datos)
+
 users = {
     'user1': '1234'
 }
+
+
 @app.route('/validate', methods=['POST'])
 def validate_user():
+    """
+    Function para validar el usuario con sus respectivas caracteristicas
+    """
     user_data = request.get_json()
     username = user_data.get('username')
     password = user_data.get('password')
-    # Validar las credenciales (simulación)
+
     if username in users and users[username] == password:
         # Generar JWT
         token = jwt.encode({
@@ -28,3 +35,20 @@ def validate_user():
         return jsonify({'token': token}), 200
     else:
         return jsonify({'error': 'Invalid credentials'}), 401
+    
+
+@app.route('/validateToken', methods=['POST'])
+def validate_token():
+    """
+    Function para realizar validacion del token cifrandolo.
+    """
+    token_recibido = request.get_json()
+    token = None
+    if token_recibido:
+        token = token_recibido.split(' ')[1]
+    token_guardado = token
+    try:
+        payload = jwt.decode(token_guardado, SECRET_KEY, algorithms=[ALGORITHM])
+        return jsonify({'mensaje': 'Token válido', 'payload': payload}), 200
+    except jwt.InvalidTokenError:
+        return jsonify({'mensaje': 'Token inválido o modificado'}), 401
