4c6849dceeaaead2bf19c8629eabc17c89839934
ekcondev <e.condev@uniandes.edu.co>
2024-09-26 19:58:21 -0500

    Funciones para consultar incidentes y validar token

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -42,6 +42,30 @@ def login():
     except requests.exceptions.RequestException as e:
         return jsonify({'error': 'Authorization service request failed', 'details': str(e)}), 502
     
+@app.route('/incidentes', methods=['POST'])
+def consulta_incidentes():
+    auth_header = request.headers.get('Authorization')
+    if validar_token(auth_header) == True:
+        return "Consulta realizada correctamente", 200
+    else:
+        return validar_token(auth_header)
+
+        
+def validar_token(token):
+    if not token:
+        return jsonify({'error': 'Token are required'}), 400
+    try:
+        # Enviar el token al microservicio autorizador
+        response = requests.post(f"{AUTHORIZER_URL}/validateToken", json=token)
+        # Verificar si la solicitud fue exitosa
+        if response.status_code == 200:
+            return True
+        else:
+            # Manejar errores del microservicio autorizador
+            return jsonify(response.json()), response.status_code
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': 'Authorization service request failed', 'details': str(e)}), 502
+
 
 if __name__ == '__main__':
     app.run(port=5000)
